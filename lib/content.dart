1e21b0e7a035ea77e8c5c1d4de45de12ee31fa3f
ElizabethYasmin <elizabethhuanca40172@gmail.com>
2024-05-24 22:36:28 -0500

    Agregando mas ventanas

--- lib/content.dart
+++ lib/content.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart'; // Asegúrate de importar el paquete CurvedNavigationBar
 import 'splashscreen.dart'; // Asegúrate de importar la pantalla Splashscreen
+import 'icono/flutterNew/my_flutter_app_icons.dart';
 
 class Contentpage extends StatefulWidget {
   const Contentpage({super.key});
@@ -13,8 +14,7 @@ class Contentpage extends StatefulWidget {
 
 class _ContentpageState extends State<Contentpage> {
   int _page = 0;
-  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey =
-      GlobalKey(); // Añade la clave global
+  GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   late ConfettiController _controllerTopCenter;
 
@@ -33,8 +33,6 @@ class _ContentpageState extends State<Contentpage> {
 
   @override
   Widget build(BuildContext context) {
-    //var w = MediaQuery.of(context).size.width;
-
     return SafeArea(
       child: Container(
         decoration: BoxDecoration(
@@ -48,15 +46,15 @@ class _ContentpageState extends State<Contentpage> {
           ),
         ),
         child: Scaffold(
-          //backgroundColor: Colors.black, // Cambiado a negro
-          backgroundColor:
-              Colors.transparent, // Hacer el fondo del Scaffold transparente
+          backgroundColor: Colors.transparent,
           bottomNavigationBar: CurvedNavigationBar(
             key: _bottomNavigationKey,
+            backgroundColor: Colors.transparent,
+            color: Color.fromARGB(86, 0, 0, 0),
             items: <Widget>[
-              Icon(Icons.add, size: 30),
-              Icon(Icons.list, size: 30),
-              Icon(Icons.compare_arrows, size: 30),
+              Icon(Icons.favorite, size: 30, color: Colors.pink),
+              Icon(MyFlutterApp.spider, size: 30, color: Colors.blue),
+              Icon(Icons.audiotrack, size: 30, color: Colors.green),
             ],
             onTap: (index) {
               setState(() {
@@ -64,119 +62,103 @@ class _ContentpageState extends State<Contentpage> {
               });
             },
           ),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: ConfettiWidget(
-                      confettiController: _controllerTopCenter,
-                      blastDirection: 0,
-                      maxBlastForce: 5,
-                      minBlastForce: 2,
-                      emissionFrequency: 0.05,
-                      numberOfParticles: 50,
-                      gravity: 1,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: ConfettiWidget(
-                      confettiController: _controllerTopCenter,
-                      blastDirection: 3.14,
-                      maxBlastForce: 5,
-                      minBlastForce: 2,
-                      emissionFrequency: 0.05, // Corregido a un valor válido
-                      numberOfParticles: 50,
-                      gravity: 1,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    //decoration: BoxDecoration(
-                    //gradient: LinearGradient(colors: [Colors.red, Colors.pink]),
-                    //borderRadius: BorderRadius.circular(10),
-                    //),
-                    child: Column(
-                      children: [
-                        Center(
-                          child: SizedBox(
-                            height: 400.0,
-                            width: 400.0,
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                Image(
-                                  image: AssetImage('assets/spidervers.gif'),
-                                  height: 900,
-                                  width: 900,
-                                ),
-                              ],
-                            ),
-                          ),
+          body: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: ConfettiWidget(
+                          confettiController: _controllerTopCenter,
+                          blastDirection: 0,
+                          maxBlastForce: 5,
+                          minBlastForce: 2,
+                          emissionFrequency: 0.05,
+                          numberOfParticles: 50,
+                          gravity: 1,
                         ),
-                        SizedBox(height: 40),
-                        //Text(
-                        //"Hey hello Fernando",
-                        //style: GoogleFonts.poppins(
-                        //    fontSize: 18, color: Colors.white),
-                        //),
-                        SizedBox(height: 10),
-                        Center(
-                          child: DefaultTextStyle(
-                            style: const TextStyle(
-                              fontSize: 50.0,
-                              fontFamily: 'Bobbers',
-                            ),
-                            child: AnimatedTextKit(
-                              animatedTexts: [
-                                TyperAnimatedText('Feliz'),
-                                TyperAnimatedText('Cumpleaños'),
-                                TyperAnimatedText('FERNANDO!!!'),
-                                TyperAnimatedText('...'),
-                              ],
-                              onTap: () {
-                                print("Tap Event");
-                              },
-                            ),
-                          ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: ConfettiWidget(
+                          confettiController: _controllerTopCenter,
+                          blastDirection: 3.14,
+                          maxBlastForce: 5,
+                          minBlastForce: 2,
+                          emissionFrequency: 0.05,
+                          numberOfParticles: 50,
+                          gravity: 1,
                         ),
-                        SizedBox(height: 20),
-                        SizedBox(
-                          width: 150.0,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: StadiumBorder(),
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => Splashscreen()));
-                            },
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Text(
-                                  "Volver",
-                                  style: TextStyle(color: Colors.red),
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Center(
+                              child: SizedBox(
+                                height: 400.0,
+                                width: 400.0,
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Image(
+                                      image: AssetImage('assets/spidervers.gif'),
+                                      height: 900,
+                                      width: 900,
+                                    ),
+                                  ],
                                 ),
-                                Icon(
-                                  Icons.arrow_back,
-                                  color: Colors.red,
-                                ),
-                              ],
+                              ),
                             ),
-                          ),
+                            SizedBox(height: 40),
+                            Center(
+                              child: DefaultTextStyle(
+                                style: const TextStyle(
+                                  fontSize: 50.0,
+                                  fontFamily: 'Bobbers',
+                                ),
+                                child: AnimatedTextKit(
+                                  animatedTexts: [
+                                    TyperAnimatedText('Feliz'),
+                                    TyperAnimatedText('Cumpleaños'),
+                                    TyperAnimatedText('FERNANDO!!!'),
+                                    TyperAnimatedText('...'),
+                                  ],
+                                  onTap: () {
+                                    print("Tap Event");
+                                  },
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+              Positioned(
+                top: 20,
+                right: 20,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => Splashscreen(),
+                    ));
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
